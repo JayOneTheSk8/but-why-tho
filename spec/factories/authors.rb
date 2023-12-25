4f35fa9d0aeda1cc9dfc434f8acd6686df531971
JayOneTheSk8 <misterjustincox@gmail.com>
2023-12-25 02:22:05 -0500

    update author specs

--- spec/factories/authors.rb
+++ spec/factories/authors.rb
@@ -2,5 +2,15 @@ FactoryBot.define do
   factory :author do
     sequence(:username) { |n| "user#{n}" }
     sequence(:email) { |n| "email#{n}@somewhere.com" }
+
+    password { "s0cRa+35" }
+
+    trait :confirmed do
+      confirmed_at { Time.current }
+    end
+
+    trait :unconfirmed do
+      confirmed_at { nil }
+    end
   end
 end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -3,7 +3,7 @@ require "rails_helper"
 RSpec.describe Author do
   let(:author) { build(:author) }
 
-  describe "valid?" do
+  describe "#valid?" do
     %i[
       email
       username
@@ -80,5 +80,117 @@ RSpec.describe Author do
     email = "Coolio1Nice@email.com"
     author.update!(email:)
     expect(author.email).to eq email.downcase
+  end
+
+  # describe "#authenticate" do
+  #   let(:password) { "coolio" }
+  #   let!(:a) { create(:author, password:) }
+
+  #   it "checks the password of the author" do
+  #     expect(a.authenticate(password)).to be_present
+  #     expect(a.authenticate("badpass")).to be_nil
+  #   end
+  # end
+
+  describe "#confirm!" do
+    context "with confirmed_at" do
+      let!(:a) { create(:author, :confirmed) }
+
+      it "does nothing" do
+        expect { a.confirm! }.not_to change(a.reload, :confirmed_at)
+      end
+    end
+
+    context "without confirmed_at" do
+      let!(:a) { create(:author, :unconfirmed) }
+      let(:now) { Time.current }
+
+      before { allow(Time).to receive(:current).and_return(now) }
+
+      it "fills in the confirmed_at column with the current time" do
+        expect { a.confirm! }
+          .to change { a.reload.confirmed_at&.strftime("%FT%T.%6N") }
+          .from(nil).to(now.strftime("%FT%T.%6N"))
+      end
+    end
+  end
+
+  describe "#confirmed?" do
+    context "with confirmed_at" do
+      let!(:a) { create(:author, :confirmed) }
+
+      it "returns true" do
+        expect(a.confirmed?).to be true
+      end
+    end
+
+    context "without confirmed_at" do
+      let!(:a) { create(:author, :unconfirmed) }
+
+      it "returns false" do
+        expect(a.confirmed?).to be false
+      end
+    end
+  end
+
+  describe "#unconfirmed?" do
+    context "with confirmed_at" do
+      let!(:a) { create(:author, :confirmed) }
+
+      it "returns false" do
+        expect(a.unconfirmed?).to be false
+      end
+    end
+
+    context "without confirmed_at" do
+      let!(:a) { create(:author, :unconfirmed) }
+
+      it "returns true" do
+        expect(a.unconfirmed?).to be true
+      end
+    end
+  end
+
+  describe "#generate_confirmation_token" do
+    context "when unconfirmed" do
+      let!(:a) { create(:author, :unconfirmed) }
+      let(:token) { a.generate_confirmation_token }
+
+      it "creates a signed id for the user's email confirmation" do
+        expect(token).to be_present
+        expect(described_class.find_signed(token, purpose: :confirm_email)).to eq a
+      end
+    end
+
+    context "when confirmed" do
+      let!(:a) { create(:author, :confirmed) }
+      let(:token) { a.generate_confirmation_token }
+
+      it "does not create a signed id" do
+        expect(token).to be_nil
+      end
+    end
+  end
+
+  describe "scopes" do
+    let!(:a1) { create(:author, :unconfirmed) }
+    let!(:a2) { create(:author, :confirmed) }
+    let!(:a3) { create(:author, :unconfirmed) }
+    let!(:a4) { create(:author, :unconfirmed) }
+    let!(:a5) { create(:author, :confirmed) }
+
+    describe ".confirmed" do
+      it "returns the confirmed authors" do
+        expect(described_class.confirmed.order(:id).pluck(:id))
+          .to eq [a2, a5].map(&:id)
+      end
+    end
+
+    describe ".unconfirmed" do
+      it "returns the unconfirmed authors" do
+        expect(described_class.unconfirmed.order(:id).pluck(:id))
+          .to eq [a1, a3, a4].map(&:id)
+      end
+    end
   end
 end
