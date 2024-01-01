e90d2b762e3023300a098e1fd59531d04c0866e1
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-01 14:53:02 -0500

    use faker for Post factory text

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :post do
-    text { "Considering the many of them online, is this the best forum to ask a question?" }
+    text { Faker::Lorem.question(word_count: rand(5..10)) }
     author_id { create(:user).id }
   end
 end
