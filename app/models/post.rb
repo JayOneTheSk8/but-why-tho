5840bc3977dee29b7c67cdd91452fd42564c8b1c
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-01 23:42:27 -0500

    use QuestionValidation in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,16 +1,5 @@
 class Post < ApplicationRecord
+  include QuestionValidation
+
   belongs_to :author, class_name: :User
-
-  validates :text, presence: true, length: {maximum: 200}
-
-  validate :text_is_question
-
-  private
-
-  def text_is_question
-    return if text.blank?
-    return unless text.last != "?" || %w[! ¡ .].any? { |chr| text.include?(chr) }
-
-    errors.add(:base, "Text must only have questions")
-  end
 end
