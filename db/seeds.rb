c31d4618735a95f89fdd5fc2730aa675ecd5cbf7
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-02 15:31:44 -0500

    add comments and reples to db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,64 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 
-user1 = User.create!(
+User.create!(
   username: "coolGuy17",
   email: "livinincalabasas@dbz.com",
   password: "winnerch1ckend1nneR",
   confirmed_at: Time.current
 )
-user2 = User.create!(
+User.create!(
   username: "dolly_parkins",
   email: "dolpar@gm.com",
-  password: "Mov1n0nup"
+  password: "Mov1n0nup",
+  confirmed_at: Time.current
+)
+User.create!(
+  username: "monsieurMongo",
+  email: "mon@mongo.com",
+  password: "un+i3d"
+)
+User.create!(
+  username: "grennaGreenz143",
+  email: "greenzzzz@more.com",
+  password: "ju5t@pasS"
 )
 
 20.times do
   Post.create!(
     text: Faker::Lorem.question(word_count: rand(5..10)),
-    author: [user1, user2].sample
+    author: User.order("RANDOM()").take
+  )
+end
+
+# Parent comments
+7.times do
+  Comment.create!(
+    text: Faker::Lorem.question(word_count: rand(5..10)),
+    author: User.order("RANDOM()").take,
+    post: Post.order("RANDOM()").take
+  )
+end
+
+# Replies to parent comments
+10.times do
+  comment = Comment.parents.order("RANDOM()").take
+  Comment.create!(
+    text: Faker::Lorem.question(word_count: rand(5..10)),
+    author: User.order("RANDOM()").take,
+    post: comment.post,
+    parent: comment
+  )
+end
+
+# Replies to replies
+replies = Comment.replies
+12.times do
+  reply = replies.order("RANDOM()").take
+  Comment.create!(
+    text: Faker::Lorem.question(word_count: rand(5..10)),
+    author: User.order("RANDOM()").take,
+    post: reply.post,
+    parent: reply
   )
 end
