34f7e261f6988ff748224e4c7799786342577972
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-04 20:02:02 -0500

    permit display name param in users#create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,14 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params
+      .require(:user)
+      .permit(
+        :username,
+        :display_name,
+        :email,
+        :password,
+        :password_confirmation
+      )
   end
 end
