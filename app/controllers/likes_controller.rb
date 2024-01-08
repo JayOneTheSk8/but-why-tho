81dcfa5e8fcea12665a94f05f9ca76492494e2c3
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-08 00:17:36 -0500

    create controller to manage and view likes * add routes

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,103 @@
+class LikesController < ApplicationController
+  before_action(
+    :require_login,
+    only: [
+      :create_comment_like,
+      :create_post_like,
+      :destroy_comment_like,
+      :destroy_post_like
+    ]
+  )
+
+  def create_comment_like
+    @like = CommentLike.new(like_params)
+    @like.user_id = current_user.id
+
+    save_like!
+  end
+
+  def create_post_like
+    @like = PostLike.new(like_params)
+    @like.user_id = current_user.id
+
+    save_like!
+  end
+
+  def destroy_comment_like
+    @like = CommentLike.find_by(
+      user_id: current_user.id,
+      message_id: params[:like][:message_id]
+    )
+
+    destroy_like!
+  end
+
+  def destroy_post_like
+    @like = PostLike.find_by(
+      user_id: current_user.id,
+      message_id: params[:like][:message_id]
+    )
+
+    destroy_like!
+  end
+
+  def user_likes
+    @user = User.find_by(id: params[:user_id])
+
+    if @user.present?
+      render(
+        json: {likes: @user.likes},
+        status: :ok
+      )
+    else
+      render(
+        json: {errors: ["Unable to find user."]},
+        status: :not_found
+      )
+    end
+  end
+
+  private
+
+  def save_like!
+    if @like.save
+      render :show
+    else
+      render(
+        json: {errors: @like.errors.full_messages},
+        status: :unprocessable_entity
+      )
+    end
+  end
+
+  def destroy_like!
+    if @like.present?
+      if @like.destroy
+        render :show
+      else
+        render(
+          json: {errors: @like.errors.full_messages},
+          status: :unprocessable_entity
+        )
+      end
+    else
+      render(
+        json: {errors: ["Unable to find like reference."]},
+        status: :not_found
+      )
+    end
+  end
+
+  def require_login
+    return if logged_in?
+
+    render(
+      json: {errors: ["Must be logged in to manage likes."]},
+      status: :unauthorized
+    )
+  end
+
+  def like_params
+    params.require(:like).permit(:message_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -13,8 +13,15 @@ Rails.application.routes.draw do
   post "follows", to: "follows#create", format: "json"
   delete "follows", to: "follows#destroy", format: "json"
 
+  post "comment_likes", to: "likes#create_comment_like", format: "json"
+  delete "comment_likes", to: "likes#destroy_comment_like", format: "json"
+
+  post "post_likes", to: "likes#create_post_like", format: "json"
+  delete "post_likes", to: "likes#destroy_post_like", format: "json"
+
   get "users/:user_id/posts", to: "posts#user_posts", format: "json"
   get "users/:user_id/comments", to: "comments#user_comments", format: "json"
   get "users/:user_id/subscriptions", to: "follows#followed_users", format: "json"
   get "users/:user_id/followers", to: "follows#followers", format: "json"
+  get "users/:user_id/likes", to: "likes#user_likes", format: "json"
 end
