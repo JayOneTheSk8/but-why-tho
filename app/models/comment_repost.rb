6fd4a1d16fab4094107a0d526a7376ebaa04aeda
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-09 01:45:38 -0500

    add validation to reposts making different user mandatory

--- app/models/comment_repost.rb
+++ app/models/comment_repost.rb
@@ -1,4 +1,15 @@
 class CommentRepost < Repost
   belongs_to :user
   belongs_to :comment, class_name: :Comment, foreign_key: :message_id
+
+  validate :other_user_content
+
+  private
+
+  def other_user_content
+    return if comment.nil?
+    return if comment.author_id != user_id
+
+    errors.add(:base, "User cannot repost own content")
+  end
 end
--- app/models/post_repost.rb
+++ app/models/post_repost.rb
@@ -1,4 +1,15 @@
 class PostRepost < Repost
   belongs_to :user
   belongs_to :post, class_name: :Post, foreign_key: :message_id
+
+  validate :other_user_content
+
+  private
+
+  def other_user_content
+    return if post.nil?
+    return if post.author_id != user_id
+
+    errors.add(:base, "User cannot repost own content")
+  end
 end
