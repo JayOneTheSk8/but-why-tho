43d0e8f89feb5cc7b5921ac11420ef652ef1057f
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-10 01:37:48 -0500

    remove unnecessary bangs in specs

--- spec/models/comment_repost_spec.rb
+++ spec/models/comment_repost_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe CommentRepost do
   describe "#valid?" do
     context "when reposter is not the author of the comment" do
       let!(:comment) { create(:comment) }
-      let!(:comment_repost) { build(:comment_repost, message_id: comment.id) }
+      let(:comment_repost) { build(:comment_repost, message_id: comment.id) }
 
       it "is valid" do
         expect(comment_repost).to be_valid
@@ -13,7 +13,7 @@ RSpec.describe CommentRepost do
 
     context "when reposter is the author of the comment" do
       let!(:comment) { create(:comment) }
-      let!(:comment_repost) { build(:comment_repost, user: comment.author, message_id: comment.id) }
+      let(:comment_repost) { build(:comment_repost, user: comment.author, message_id: comment.id) }
 
       it "is not valid" do
         expect(comment_repost).not_to be_valid
--- spec/models/post_repost_spec.rb
+++ spec/models/post_repost_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe PostRepost do
   describe "#valid?" do
     context "when reposter is not the author of the post" do
       let!(:post) { create(:post) }
-      let!(:post_repost) { build(:post_repost, message_id: post.id) }
+      let(:post_repost) { build(:post_repost, message_id: post.id) }
 
       it "is valid" do
         expect(post_repost).to be_valid
@@ -13,7 +13,7 @@ RSpec.describe PostRepost do
 
     context "when reposter is the author of the post" do
       let!(:post) { create(:post) }
-      let!(:post_repost) { build(:post_repost, user: post.author, message_id: post.id) }
+      let(:post_repost) { build(:post_repost, user: post.author, message_id: post.id) }
 
       it "is not valid" do
         expect(post_repost).not_to be_valid
