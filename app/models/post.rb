4ce27d1ee5615784b758f2f13f31a98842192f7f
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-27 22:04:00 -0500

    add method to get full post data

--- app/models/post.rb
+++ app/models/post.rb
@@ -266,4 +266,219 @@ class Post < ApplicationRecord
       }
     end
   end
+
+  def get_data(current_user: nil)
+    binds = [
+      ActiveRecord::Relation::QueryAttribute.new(
+        "post_id",
+        id,
+        ActiveRecord::Type::Integer.new
+      ),
+      ActiveRecord::Relation::QueryAttribute.new(
+        "current_user_id",
+        current_user&.id || 0,
+        ActiveRecord::Type::Integer.new
+      )
+    ]
+
+    sql_results =
+      ActiveRecord::Base.connection.select_all(<<~SQL.squish, "Following Posts", binds).to_a
+        WITH current_user_reposts as (
+          SELECT
+            reposts.id as repost_id,
+            reposts.created_at as reposted_at,
+            reposts.type as repost_type,
+            reposts.message_id as message_id,
+            reposter.display_name as reposter_display_name
+          FROM reposts
+          INNER JOIN users reposter
+            ON reposter.id = reposts.user_id
+          WHERE reposts.user_id = $2
+        ), current_user_likes as (
+          SELECT
+            likes.id as like_id,
+            likes.created_at as liked_at,
+            likes.type as like_type,
+            likes.message_id as message_id
+          FROM likes
+          WHERE likes.user_id = $2
+        ), current_user_subscriptions as (
+          SELECT
+            follows.id as follow_id,
+            follows.followee_id as followee_id
+          FROM follows
+          WHERE follows.follower_id = $2
+        ), post_data as (
+          SELECT
+            posts.id as id,
+            posts.text as text,
+            posts.created_at as created_at,
+            post_authors.id as author_id,
+            post_authors.username as author_username,
+            post_authors.display_name as author_display_name,
+            'PostRepost' as repost_type,
+            'PostLike' as like_type,
+            'Post' as show_type,
+            (
+              CASE
+              WHEN current_user_likes.like_id IS NOT NULL
+                THEN TRUE
+              ELSE
+                FALSE
+              END
+            ) user_liked,
+            (
+              CASE
+              WHEN current_user_reposts.repost_id IS NOT NULL
+                THEN TRUE
+              ELSE
+                FALSE
+              END
+            ) user_reposted,
+            (
+              CASE
+              WHEN current_user_subscriptions.follow_id IS NOT NULL
+                THEN TRUE
+              ELSE
+                FALSE
+              END
+            ) user_followed,
+            COUNT(
+              CASE
+              WHEN post_comments.parent_id IS NULL
+                THEN post_comments.id
+              END
+            ) as comment_count
+          FROM posts
+          INNER JOIN users post_authors
+            ON posts.author_id = post_authors.id
+          LEFT OUTER JOIN comments post_comments
+            ON post_comments.post_id = posts.id
+          LEFT OUTER JOIN current_user_likes
+            ON current_user_likes.like_type = 'PostLike'
+            AND current_user_likes.message_id = posts.id
+          LEFT OUTER JOIN current_user_reposts
+            ON current_user_reposts.repost_type = 'PostRepost'
+            AND current_user_reposts.message_id = posts.id
+          LEFT OUTER JOIN current_user_subscriptions
+            ON current_user_subscriptions.followee_id = posts.author_id
+          WHERE posts.id = $1
+          GROUP BY
+            posts.id,
+            post_authors.id,
+            current_user_likes.like_id,
+            current_user_reposts.repost_id,
+            current_user_subscriptions.follow_id
+        ), post_comment_data as (
+          SELECT
+            comments.id as id,
+            comments.text as text,
+            comments.created_at as created_at,
+            comment_authors.id as author_id,
+            comment_authors.username as author_username,
+            comment_authors.display_name as author_display_name,
+            'CommentRepost' as repost_type,
+            'CommentLike' as like_type,
+            'Comment' as show_type,
+            (
+              CASE
+              WHEN current_user_likes.like_id IS NOT NULL
+                THEN TRUE
+              ELSE
+                FALSE
+              END
+            ) user_liked,
+            (
+              CASE
+              WHEN current_user_reposts.repost_id IS NOT NULL
+                THEN TRUE
+              ELSE
+                FALSE
+              END
+            ) user_reposted,
+            (
+              CASE
+              WHEN current_user_subscriptions.follow_id IS NOT NULL
+                THEN TRUE
+              ELSE
+                FALSE
+              END
+            ) user_followed,
+            COUNT(comment_replies.id) as comment_count
+          FROM comments
+          INNER JOIN users comment_authors
+            ON comments.author_id = comment_authors.id
+          LEFT OUTER JOIN comments comment_replies
+            ON comment_replies.parent_id = comments.id
+          LEFT OUTER JOIN current_user_likes
+            ON current_user_likes.like_type = 'CommentLike'
+            AND current_user_likes.message_id = comments.id
+          LEFT OUTER JOIN current_user_reposts
+            ON current_user_reposts.repost_type = 'CommentRepost'
+            AND current_user_reposts.message_id = comments.id
+          LEFT OUTER JOIN current_user_subscriptions
+            ON current_user_subscriptions.followee_id = comments.author_id
+          WHERE comments.post_id = $1 AND comments.parent_id IS NULL
+          GROUP BY
+            comments.id,
+            comment_authors.id,
+            current_user_likes.like_id,
+            current_user_reposts.repost_id,
+            current_user_subscriptions.follow_id
+        ), merged as (
+          SELECT * FROM post_data
+          UNION ALL
+          SELECT * FROM post_comment_data
+        ), like_counts as (
+          SELECT
+            COUNT(likes.id) as like_count,
+            likes.message_id as message_id,
+            likes.type as message_type
+          FROM likes
+          INNER JOIN merged
+            ON likes.type = merged.like_type
+            AND likes.message_id = merged.id
+          GROUP BY
+            likes.type, likes.message_id
+        ), repost_counts as (
+          SELECT
+            COUNT(reposts.id) as repost_count,
+            reposts.message_id as message_id,
+            reposts.type as message_type
+          FROM reposts
+          INNER JOIN merged
+            ON reposts.type = merged.repost_type
+            AND reposts.message_id = merged.id
+          GROUP BY
+            reposts.type, reposts.message_id
+        )
+        SELECT
+          merged.*,
+          COALESCE(like_counts.like_count, 0) as like_count,
+          COALESCE(repost_counts.repost_count, 0) as repost_count
+        FROM merged
+        LEFT OUTER JOIN like_counts
+          ON like_counts.message_type = merged.like_type
+          AND like_counts.message_id = merged.id
+        LEFT OUTER JOIN repost_counts
+          ON repost_counts.message_type = merged.repost_type
+          AND repost_counts.message_id = merged.id
+        ORDER BY merged.created_at DESC
+      SQL
+
+    results = {
+      comments: []
+    }
+
+    sql_results.each do |result|
+      case result["show_type"].to_sym
+      when :Post
+        results.merge!(map_sql_to_message(result))
+      when :Comment
+        results[:comments] << map_sql_to_message(result)
+      end
+    end
+
+    results
+  end
 end
