6663d3d616cad195b478feea7682f5107b0f9532
JayOneTheSk8 <misterjustincox@gmail.com>
2024-01-27 22:08:48 -0500

    map comment and post data results

--- app/models/concerns/question_validation.rb
+++ app/models/concerns/question_validation.rb
@@ -8,6 +8,25 @@ module QuestionValidation
 
   private
 
+  def map_sql_to_message(result)
+    {
+      id: result["id"],
+      text: result["text"],
+      created_at: result["created_at"],
+      comment_count: result["comment_count"],
+      like_count: result["like_count"],
+      repost_count: result["repost_count"],
+      user_liked: result["user_liked"],
+      user_reposted: result["user_reposted"],
+      user_followed: result["user_followed"],
+      author: {
+        id: result["author_id"],
+        username: result["author_username"],
+        display_name: result["author_display_name"]
+      }
+    }
+  end
+
   def text_is_question
     return if text.blank?
     return unless text.last != "?" || %w[! ¡ .].any? { |chr| text.include?(chr) }
